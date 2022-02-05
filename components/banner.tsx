f54c2b60b4c0766f55ecc5d055570a811044ec7d
silver <quangtrung20002012@gmail.com>
2022-02-05 15:28:20 +0700

    end lesson 21

---
+++ components/banner.tsx
@@ -0,0 +1,25 @@
+import React from "react";
+import styles from "./banner.module.css";
+
+const Banner = ({
+  buttonText,
+  handleOnClick,
+}: {
+  buttonText: string;
+  handleOnClick: () => void;
+}) => {
+  return (
+    <div className={styles.container}>
+      <h1>
+        <span className={styles.title1}>Coffee</span>
+        <span className={styles.title2}>Connoisseur</span>
+      </h1>
+      <p className={styles.subTitle}>Discover your local coffee shops!</p>
+      <button className={styles.button} onClick={handleOnClick}>
+        {buttonText}
+      </button>
+    </div>
+  );
+};
+
+export default Banner;
